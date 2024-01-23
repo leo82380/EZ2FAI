fd121696a1daaa496e4dea6430fa7345d2cf23fb
LEE SANG GYU <tkdrb062@gmail.com>
2024-01-24 03:10:02 +0900

    fix: fps

---
+++ EZ2FAI/MPSet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class MPSet : MonoBehaviour
+{
+    private bool isDeleteCheck = false;
+    void Update()
+    {
+        try
+        {
+            if (scrConductor.instance == null || Main.mp == null)
+            {
+                return;
+            }
+            if (scrConductor.instance.isGameWorld && !scrConductor.isEditingLevel && !scrConductor.isOfficialLevel)
+            {
+                Main.mp.SetActive(true);
+                
+                Main.mp.transform.GetChild(0).position = new Vector2(Main.X * Screen.width, Main.Y * Screen.height);
+                Main.mp.transform.GetChild(0).localScale = new Vector3(Main.S, Main.S, 1f);
+                DeleteCheckPoint();
+
+            }
+            else
+            {
+                Main.mp.SetActive(false);
+                isDeleteCheck = false;
+            }
+        }
+        catch (Exception e) 
+        {
+            Debug.Log("=======================================");
+            Debug.Log(e.Message);
+            return;
+        }
+
+    }
+
+    void DeleteCheckPoint()
+    {
+        if (isDeleteCheck) return;
+        var checkPoints = FindObjectsOfType<ffxCheckpoint>();
+        if (checkPoints == null) return;
+        foreach (var checkPoint in checkPoints)
+        {
+            checkPoint.floor.floorIcon = FloorIcon.None;
+        }
+        isDeleteCheck = true;
+    }
+}
---
+++ EZ2FAI/RandomStringGenerator.cs
@@ -0,0 +1,17 @@
+﻿public class RandomStringGenerator
+{
+    public static readonly System.Random random = new System.Random();
+    public const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static string GenerateRandomString(int length)
+    {
+        char[] randomString = new char[length];
+
+        for(int i = 0;i < length;i++)
+        {
+            randomString[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new string(randomString);
+    }
+}
